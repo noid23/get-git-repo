de9c0ec16e99f16c6ee5edea3a2a4a2afc9aa6d0
Dave Null <noid23@gmail.com>
2021-05-04 16:56:24 -0700

    upload script**

---
+++ get-git-repo.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+if [ $# -eq 0 ]; then
+	echo "Usage: ./get-git-repo.sh GITHUBUSER"
+	exit 1
+elif [ $# -gt 0 ]; then
+	githubuser=$1;
+	printf "Pulling repos for $githubuser\n";
+	/usr/bin/curl -s https://api.github.com/users/$githubuser/repos?per_page=500 | /usr/bin/jq -r '.[] | .html_url' > $githubuser-repos.txt;
+	repos=$(cat $githubuser-repos.txt | wc -l);
+	printf "Collected $repos repos from user $githubuser and put them in $githubuser-repos.txt\n"
+fi
