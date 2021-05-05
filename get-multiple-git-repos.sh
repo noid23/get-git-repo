b4300f506df61cf00e9e29901da70d6f0bdb6eea
Dave Null <noid23@gmail.com>
2021-05-05 10:51:04 -0700

    Simplified var names

--- get-multiple-git-repos.sh
+++ get-multiple-git-repos.sh
@@ -1,10 +1,9 @@
 #/bin/bash
 if [ -n "$1" ]; then
-	for param in "$@" ; do
-		githubuser=$param;
+	for githubuser in "$@" ; do
         	printf "Pulling repos for $githubuser\n";
         	/usr/bin/curl -s https://api.github.com/users/$githubuser/repos?per_page=500 | /usr/bin/jq -r '.[] | .html_url' > ~/$githubuser-repos.txt;
-        	repos=$(cat $githubuser-repos.txt | wc -l);
+        	repos=$(cat ~/$githubuser-repos.txt | wc -l);
         	printf "Collected $repos repos from user $githubuser and put them in ~/$githubuser-repos.txt\n"
 	done
 else
